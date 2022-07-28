4bf9df6a5a4e9f9c131b4f5772b0261e203af541
Alexkarav98 <djmyaso@mail.ru>
2022-07-28 08:29:10 +0300

    Added user model

---
+++ itMentors/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  itMentors
+//
+//  Created by alexkarav on 28.07.2022.
+//
+
+import Foundation
+
+
+struct User {
+    let login: String
+    let password: String
+    let pathToImage: String?
+    let preferedDirection: String?
+}
