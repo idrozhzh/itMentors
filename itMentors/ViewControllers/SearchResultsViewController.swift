a4c8ca1f41c997b3b7e2ae9a930c34274fc29ac3
i_drozhzh <idrozhzh@gmail.com>
2022-07-31 13:27:38 +0300

    Added SearchResultsViewController class file

---
+++ itMentors/ViewControllers/SearchResultsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultsViewController.swift
+//  itMentors
+//
+//  Created by Иван Дрожжин on 31.07.2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
